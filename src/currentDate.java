7d3630e5de5c857fefe1f71a28cb042a7cd8ae1a
Anjaneyulu-Tech <designdazz124@gmail.com>
2022-10-12 19:01:15 +0530

    First Commit

--- src/currentDate.java
+++ src/currentDate.java
@@ -1,8 +1,9 @@
 import java.util.Date;
 
 public class currentDate {
-    public static void main (string[] args) {
+    public static void main (String[] args) {
         Date number = new Date();
         System.out.println(number);
+        System.out.println("This my first test with git hub");
     }
 }
